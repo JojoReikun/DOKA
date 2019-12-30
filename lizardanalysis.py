1f43fe2cab11d8470347e3fdac45147fea1524c9
Jojo <jojo.schultz@outlook.de>
2019-12-31 00:17:50 +1000

    added instructions about how to debug in pycharm into lizardanalysis.py

--- lizardanalysis.py
+++ lizardanalysis.py
@@ -2,6 +2,15 @@
 LizardDLCAnalysis Toolbox
 © Jojo S.
 Licensed under MIT License
+
+----------------------------------------------------------
+for testing and debugging in pycharm:
+---> Tools
+---> Python Console
+---> (with ipython installed):
+IN[1]: import lizardanalysis
+---> run commands:
+IN[2]: lizardanalysis.command(*args, **kwargs)
 """
 
 from lizardanalysis import cli
