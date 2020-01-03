d066bd007401887c55cceff05739b43136b34686
Jojo <jojo.schultz@outlook.de>
2020-01-03 21:12:55 +1000

    working in read_in_files -> check available labels -> filepath problems

--- lizardanalysis/calculations/read_in_files.py
+++ lizardanalysis/calculations/read_in_files.py
@@ -29,13 +29,23 @@ def read_csv_files(config, separate_gravity_file=False):
     print("Config file read successfully.")
 
     # get the file paths from the config file
-    files = cfg['file_sets'].keys()
-    print("files (keys): ,", files)
+    project_path = cfg['project_path']
+
+    files = cfg['file_sets'].keys()   # object type ('CommentedMapKeysView' object), does not support indexing
+    filelist = []   # store filepaths as list
+    for file in files:
+        filelist.append(file)
+    #print("files (keys): ,", filelist)  # TEST
 
     # check if user entered camera specs in config file
-    if cfg['framerate'] == '' and cfg['shutter'] == '':
+    if cfg['framerate'] is None and cfg['shutter'] is None:
         print('Please add camera settings in the config file before you continue.')
         return
+    else:
+        print('Camera settings entered: \n',
+              'framerate: ', cfg['framerate'], '\n',
+              'shutter: ', cfg['shutter'], '\n',
+              'Available labels will be checked and written to config file...')
 
     # check if user set separate gravity file to True
     if separate_gravity_file:
@@ -43,5 +53,22 @@ def read_csv_files(config, separate_gravity_file=False):
         gravity_filepath = filedialog.askopenfilename(filetype=[('csv files', '*.csv')])  # show an "Open" dialog box and return the path to the selected file
         df_gravity = pd.read_csv(gravity_filepath)   # read in gravity file
 
+    # check available labels in .csv files and write them to config file
+    # TODO: Check if working directory differs from default and set path respectively
+    # if working_directory = DEFAULT:
+    current_path = os.getcwd()
+    print('current_path ', current_path)
+    print('project_path ', project_path)
+    project_dir = '\\{pn}-{exp}-{spec}-{date}'.format(pn=cfg['Task'], exp=cfg['scorer'], spec=cfg['species'], date=cfg['date'])
+    label_file_path = project_dir + '\\files\\' + os.path.basename(filelist[0])
+    print('label_file_path: ', label_file_path)
+    rel_label_file_path = os.path.relpath(label_file_path)
+    # else:
+    # TODO: look for working directory
+    rel_label_file_path = os.path.relpath(label_file_path)
+    print('relative label file path: ', rel_label_file_path)
+    data = pd.read_csv(rel_label_file_path)
+    data.head()
+    #print('labels written to config file.')
 
 
