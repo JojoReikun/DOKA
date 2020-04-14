bf320cd40c49d98ff58840cc4b9699ea241df48d
Jojo <jojo.schultz@outlook.de>
2020-04-14 12:09:47 +1000

    continued on summary results

--- lizardanalysis/calculations/write_result_files.py
+++ lizardanalysis/calculations/write_result_files.py
@@ -29,18 +29,18 @@ def write_summary_result_files(config):
 
     filelist = glob.glob(os.path.join(result_file_path, "*.csv"))
     filelist_split = [x.rsplit(os.sep, 1)[1] for x in filelist]
-    print(" + ", len(filelist_split), *filelist_split, sep='\n + ')
+    #print(" + ", len(filelist_split), *filelist_split, sep='\n + ')
 
     # ----------------------------------------------------------------------------------------------------------
     # options: sort by species, and optional by direction
-    filter = {'species': True, 'direction': False}
+    filter = {'species': True, 'direction': True}
 
     # loop through all individual result files to generate summary
     speciesnames = []
     for file in filelist_split:
         speciesname = species_name_split(file)
         speciesnames.append(speciesname[0])         # extracts first bit of filename until first number for species name
-    print(speciesnames)
+    #print(speciesnames)
     speciescount = dict(Counter(speciesnames))    # unique species names & count
     print("species summary: ", speciescount)
 
@@ -85,14 +85,32 @@ class species_summary:
         import pandas as pd
         import os
         import numpy as np
-        from statistics import mean, stdev
 
         retval = {}
+        toe_angles = False
 
         direction_up_counter = 0
         direction_down_counter = 0
-        deflection_species = []
-        speed_species = []
+        deflection_species_UP = []
+        deflection_species_DOWN = []
+        speed_species_UP = []
+        speed_species_DOWN = []
+        wrist_angles_fore_UP = []
+        wrist_angles_fore_DOWN = []
+        wrist_angles_hind_UP = []
+        wrist_angles_hind_DOWN = []
+        limb_ROM_fore_UP = []
+        limb_ROM_fore_DOWN = []
+        limb_ROM_hind_UP = []
+        limb_ROM_hind_DOWN = []
+        spine_ROM_UP = []
+        spine_ROM_DOWN = []
+        CROM_fore_UP = []
+        CROM_fore_DOWN = []
+        CROM_hind_UP = []
+        CROM_hind_DOWN = []
+        toe_angle_sum_UP = []
+        toe_angle_sum_DOWN = []
 
         # >>>>> read in all csv files belonging to the species:
         for file in self.filelist_filtered:
@@ -109,28 +127,170 @@ class species_summary:
             else:
                 pass
 
-            # deflection ---------------------------------------------------------- :
-            deflection_species.append(list(data['body_deflection_angle']))
 
-            # speed --------------------------------------------------------------- :
-            speed_species.append(list(data['speed_PXperS']))
+            if direction == "UP":
+                # deflection ---------------------------------------------------------- :
+                deflection_species_UP.append(list(data['body_deflection_angle']))
+                # speed --------------------------------------------------------------- :
+                speed_species_UP.append(list(data['speed_PXperS']))
+                # wrist angles -------------------------------------------------------- :
+                wrist_angles_fore_UP.append(list(data['mid_stance_wrist_angles_mean_FL']))
+                wrist_angles_fore_UP.append(list(data['mid_stance_wrist_angles_mean_FR']))
+                wrist_angles_hind_UP.append(list(data['mid_stance_wrist_angles_mean_HL']))
+                wrist_angles_hind_UP.append(list(data['mid_stance_wrist_angles_mean_HR']))
+                # limb ROM ------------------------------------------------------------ :
+                limb_ROM_fore_UP.append(list(data['limbROM_FL']))
+                limb_ROM_fore_UP.append(list(data['limbROM_FR']))
+                limb_ROM_hind_UP.append(list(data['limbROM_HR']))
+                limb_ROM_hind_UP.append(list(data['limbROM_HL']))
+                # spine ROM ----------------------------------------------------------- :
+                spine_ROM_UP.append(list(data['spineROM_FL']))
+                spine_ROM_UP.append(list(data['spineROM_FR']))
+                spine_ROM_UP.append(list(data['spineROM_HR']))
+                spine_ROM_UP.append(list(data['spineROM_HL']))
+                # CROM ---------------------------------------------------------------- :
+                CROM_fore_UP.append(list(data['CROM_FL']))
+                CROM_fore_UP.append(list(data['CROM_FR']))
+                CROM_hind_UP.append(list(data['CROM_HR']))
+                CROM_hind_UP.append(list(data['CROM_HL']))
+                if toe_angles == True:
+                    # toe angle sum ------------------------------------------------------- :
+                    toe_angle_sum_UP.append(list(data['fl_ti-fl_ti1']))
+                    toe_angle_sum_UP.append(list(data['fl_ti1-fl_to1']))
+                    toe_angle_sum_UP.append(list(data['fl_to1-fl_to']))
+                    toe_angle_sum_UP.append(list(data['fr_ti-fr_ti1']))
+                    toe_angle_sum_UP.append(list(data['fr_ti1-fr_to1']))
+                    toe_angle_sum_UP.append(list(data['fr_to1-fr_to']))
+
+            else:
+                # deflection ---------------------------------------------------------- :
+                deflection_species_DOWN.append(list(data['body_deflection_angle']))
+                # speed --------------------------------------------------------------- :
+                speed_species_DOWN.append(list(data['speed_PXperS']))
+                # wrist angles -------------------------------------------------------- :
+                wrist_angles_fore_DOWN.append(list(data['mid_stance_wrist_angles_mean_FL']))
+                wrist_angles_fore_DOWN.append(list(data['mid_stance_wrist_angles_mean_FR']))
+                wrist_angles_hind_DOWN.append(list(data['mid_stance_wrist_angles_mean_HL']))
+                wrist_angles_hind_DOWN.append(list(data['mid_stance_wrist_angles_mean_HR']))
+                # limb ROM ------------------------------------------------------------ :
+                limb_ROM_fore_DOWN.append(list(data['limbROM_FL']))
+                limb_ROM_fore_DOWN.append(list(data['limbROM_FR']))
+                limb_ROM_hind_DOWN.append(list(data['limbROM_HR']))
+                limb_ROM_hind_DOWN.append(list(data['limbROM_HL']))
+                # spine ROM ----------------------------------------------------------- :
+                spine_ROM_DOWN.append(list(data['spineROM_FL']))
+                spine_ROM_DOWN.append(list(data['spineROM_FR']))
+                spine_ROM_DOWN.append(list(data['spineROM_HR']))
+                spine_ROM_DOWN.append(list(data['spineROM_HL']))
+                # CROM ---------------------------------------------------------------- :
+                CROM_fore_DOWN.append(list(data['CROM_FL']))
+                CROM_fore_DOWN.append(list(data['CROM_FR']))
+                CROM_hind_DOWN.append(list(data['CROM_HR']))
+                CROM_hind_DOWN.append(list(data['CROM_HL']))
+                if toe_angles == True:
+                    # toe angle sum ------------------------------------------------------- :
+                    toe_angle_sum_DOWN.append(list(data['hl_ti-hl_ti1']))
+                    toe_angle_sum_DOWN.append(list(data['hl_ti1-hl_to1']))
+                    toe_angle_sum_DOWN.append(list(data['hl_to1-hl_to']))
+                    toe_angle_sum_DOWN.append(list(data['hr_ti-hr_ti1']))
+                    toe_angle_sum_DOWN.append(list(data['hr_ti1-hr_to1']))
+                    toe_angle_sum_DOWN.append(list(data['hr_to1-hr_to']))
+
 
         # >>>>> mean and stds:
+        # TODO: surely there is a better way to do this :)
         # deflection ---------------------------------------------------------- :
-        deflection_species_flattened = [element for sublist in deflection_species for element in sublist]
-        deflection_species_mean = np.nanmean(deflection_species_flattened)
-        deflection_species_std = np.nanstd(deflection_species_flattened)
+        deflection_species_flattened_UP = [element for sublist in deflection_species_UP for element in sublist]
+        deflection_species_mean_UP = np.nanmean(deflection_species_flattened_UP)
+        deflection_species_std_UP = np.nanstd(deflection_species_flattened_UP)
+        deflection_species_flattened_DOWN = [element for sublist in deflection_species_DOWN for element in sublist]
+        deflection_species_mean_DOWN = np.nanmean(deflection_species_flattened_DOWN)
+        deflection_species_std_DOWN = np.nanstd(deflection_species_flattened_DOWN)
         # speed --------------------------------------------------------------- :
-        speed_species_flattened = [element for sublist in speed_species for element in sublist]
-        speed_species_mean = np.nanmean(speed_species_flattened)
-        speed_species_std = np.nanstd(speed_species_flattened)
+        speed_species_flattened_UP = [element for sublist in speed_species_UP for element in sublist]
+        speed_species_mean_UP = np.nanmean(speed_species_flattened_UP)
+        speed_species_std_UP = np.nanstd(speed_species_flattened_UP)
+        speed_species_flattened_DOWN = [element for sublist in speed_species_DOWN for element in sublist]
+        speed_species_mean_DOWN = np.nanmean(speed_species_flattened_DOWN)
+        speed_species_std_DOWN = np.nanstd(speed_species_flattened_DOWN)
+        # wrist angles -------------------------------------------------------- :
+        wrist_angles_fore_flattened_UP = [element for sublist in wrist_angles_fore_UP for element in sublist]
+        wrist_angles_hind_flattened_UP = [element for sublist in wrist_angles_hind_UP for element in sublist]
+        wrist_angles_fore_mean_UP = np.nanmean(wrist_angles_fore_flattened_UP)
+        wrist_angles_fore_std_UP = np.nanstd(wrist_angles_fore_flattened_UP)
+        wrist_angles_hind_mean_UP = np.nanmean(wrist_angles_hind_flattened_UP)
+        wrist_angles_hind_std_UP = np.nanstd(wrist_angles_hind_flattened_UP)
+        wrist_angles_fore_flattened_DOWN = [element for sublist in wrist_angles_fore_DOWN for element in sublist]
+        wrist_angles_hind_flattened_DOWN = [element for sublist in wrist_angles_hind_DOWN for element in sublist]
+        wrist_angles_fore_mean_DOWN = np.nanmean(wrist_angles_fore_flattened_DOWN)
+        wrist_angles_fore_std_DOWN = np.nanstd(wrist_angles_fore_flattened_DOWN)
+        wrist_angles_hind_mean_DOWN = np.nanmean(wrist_angles_hind_flattened_DOWN)
+        wrist_angles_hind_std_DOWN = np.nanstd(wrist_angles_hind_flattened_DOWN)
+        # limb ROM ------------------------------------------------------------ :
+        limb_ROM_fore_flattened_UP = [element for sublist in limb_ROM_fore_UP for element in sublist]
+        limb_ROM_hind_flattened_UP = [element for sublist in limb_ROM_hind_UP for element in sublist]
+        limb_ROM_fore_mean_UP = np.nanmean(limb_ROM_fore_flattened_UP)
+        limb_ROM_fore_std_UP = np.nanstd(limb_ROM_fore_flattened_UP)
+        limb_ROM_hind_mean_UP = np.nanmean(limb_ROM_hind_flattened_UP)
+        limb_ROM_hind_std_UP = np.nanstd(limb_ROM_hind_flattened_UP)
+        limb_ROM_fore_flattened_DOWN = [element for sublist in limb_ROM_fore_DOWN for element in sublist]
+        limb_ROM_hind_flattened_DOWN = [element for sublist in limb_ROM_hind_DOWN for element in sublist]
+        limb_ROM_fore_mean_DOWN = np.nanmean(limb_ROM_fore_flattened_DOWN)
+        limb_ROM_fore_std_DOWN = np.nanstd(limb_ROM_fore_flattened_DOWN)
+        limb_ROM_hind_mean_DOWN = np.nanmean(limb_ROM_hind_flattened_DOWN)
+        limb_ROM_hind_std_DOWN = np.nanstd(limb_ROM_hind_flattened_DOWN)
+        # spine ROM ------------------------------------------------------------ :
+        spine_ROM_flattened_UP = [element for sublist in spine_ROM_UP for element in sublist]
+        spine_ROM_flattened_DOWN = [element for sublist in spine_ROM_DOWN for element in sublist]
+        spine_ROM_mean_UP = np.nanmean(spine_ROM_flattened_UP)
+        spine_ROM_std_UP = np.nanstd(spine_ROM_flattened_UP)
+        spine_ROM_mean_DOWN = np.nanmean(spine_ROM_flattened_DOWN)
+        spine_ROM_std_DOWN = np.nanstd(spine_ROM_flattened_DOWN)
+        # CROM ------------------------------------------------------------ :
+        CROM_fore_flattened_UP = [element for sublist in CROM_fore_UP for element in sublist]
+        CROM_hind_flattened_UP = [element for sublist in CROM_hind_UP for element in sublist]
+        CROM_fore_mean_UP = np.nanmean(CROM_fore_flattened_UP)
+        CROM_fore_std_UP = np.nanstd(CROM_fore_flattened_UP)
+        CROM_hind_mean_UP = np.nanmean(CROM_hind_flattened_UP)
+        CROM_hind_std_UP = np.nanstd(CROM_hind_flattened_UP)
+        CROM_fore_flattened_DOWN = [element for sublist in CROM_fore_DOWN for element in sublist]
+        CROM_hind_flattened_DOWN = [element for sublist in CROM_hind_DOWN for element in sublist]
+        CROM_fore_mean_DOWN = np.nanmean(CROM_fore_flattened_DOWN)
+        CROM_fore_std_DOWN = np.nanstd(CROM_fore_flattened_DOWN)
+        CROM_hind_mean_DOWN = np.nanmean(CROM_hind_flattened_DOWN)
+        CROM_hind_std_DOWN = np.nanstd(CROM_hind_flattened_DOWN)
+        # toe angle sum ------------------------------------------------------- :
+        toe_angle_sum_UP = [element for sublist in toe_angle_sum_UP for element in sublist]
+        toe_angle_sum_DOWN = [element for sublist in toe_angle_sum_DOWN for element in sublist]
+        toe_angle_sum_mean_UP = np.nanmean(toe_angle_sum_UP)
+        toe_angle_sum_std_UP = np.nanstd(toe_angle_sum_UP)
+        toe_angle_sum_mean_DOWN = np.nanmean(toe_angle_sum_DOWN)
+        toe_angle_sum_std_DOWN = np.nanstd(toe_angle_sum_DOWN)
+
 
         # >>>>> TEST PRINTS:
-        print("UP: {} | DOWN: {} ".format(direction_up_counter, direction_down_counter))
-        print("deflection mean: {0:.2f} ".format(deflection_species_mean),
-              "deflection std: {0:.2f}".format(deflection_species_std),
-              "\nspeed mean: {0:.2f} ".format(speed_species_mean),
-              "speed std: {0:.2f}".format(speed_species_std))
+        print("UP: {}                                                   | DOWN: {} ".format(direction_up_counter, direction_down_counter))
+        print("deflection mean UP: {0:.2f} ".format(deflection_species_mean_UP), "deflection std UP: {0:.2f}".format(deflection_species_std_UP), "       | ",
+              "deflection mean DOWN: {0:.2f} ".format(deflection_species_mean_DOWN), "deflection std DOWN: {0:.2f}".format(deflection_species_std_DOWN),
+              "\nspeed mean UP: {0:.2f} ".format(speed_species_mean_UP), "speed std UP: {0:.2f}".format(speed_species_std_UP), "           | ",
+              "speed mean DOWN: {0:.2f} ".format(speed_species_mean_DOWN), "speed std DOWN: {0:.2f}".format(speed_species_std_DOWN),
+              "\nwrist fore mean UP: {0:.2f} ".format(wrist_angles_fore_mean_UP), "wrist fore std UP: {0:.2f}".format(wrist_angles_fore_std_UP), "     | ",
+              "wrist fore mean DOWN: {0:.2f} ".format(wrist_angles_fore_mean_DOWN), "wrist fore std DOWN: {0:.2f}".format(wrist_angles_fore_std_DOWN),
+              "\nwrist hind mean UP: {0:.2f} ".format(wrist_angles_hind_mean_UP), "wrist hind std UP: {0:.2f}".format(wrist_angles_hind_std_UP), "     | ",
+              "wrist hind mean DOWN: {0:.2f} ".format(wrist_angles_hind_mean_DOWN), "wrist hind std DOWN: {0:.2f}".format(wrist_angles_hind_std_DOWN),
+              "\nlimb ROM fore mean UP: {0:.2f} ".format(limb_ROM_fore_mean_UP), "limb ROM fore std UP: {0:.2f}".format(limb_ROM_fore_std_UP), " | ",
+              "limb ROM fore mean DOWN: {0:.2f} ".format(limb_ROM_fore_mean_DOWN), "limb ROM fore std DOWN: {0:.2f}".format(limb_ROM_fore_std_DOWN),
+              "\nlimb ROM hind mean UP: {0:.2f} ".format(limb_ROM_hind_mean_UP), "limb ROM hind std UP: {0:.2f}".format(limb_ROM_hind_std_UP), " | ",
+              "limb ROM hind mean DOWN: {0:.2f} ".format(limb_ROM_hind_mean_DOWN), "limb ROM hind std DOWN: {0:.2f}".format(limb_ROM_hind_std_DOWN),
+              "\nspine ROM mean UP: {0:.2f} ".format(spine_ROM_mean_UP), "spine ROM std UP: {0:.2f}".format(spine_ROM_std_UP), "       | ",
+              "spine ROM mean DOWN: {0:.2f} ".format(spine_ROM_mean_DOWN), "spine ROM std DOWN: {0:.2f}".format(spine_ROM_std_DOWN),
+              "\nCROM fore mean UP: {0:.2f} ".format(CROM_fore_mean_UP), "CROM fore std UP: {0:.2f}".format(CROM_fore_std_UP), "        | ",
+              "CROM fore mean DOWN: {0:.2f} ".format(CROM_fore_mean_DOWN), "CROM fore std DOWN: {0:.2f}".format(CROM_fore_std_DOWN),
+              "\nCROM hind mean UP: {0:.2f} ".format(CROM_hind_mean_UP), "CROM hind std UP: {0:.2f}".format(CROM_hind_std_UP), "       | ",
+              "CROM hind mean DOWN: {0:.2f} ".format(CROM_hind_mean_DOWN), "CROM hind std DOWN: {0:.2f}".format(CROM_hind_std_DOWN),
+              "\ntoe_angle_sum mean UP: {0:.2f} ".format(toe_angle_sum_mean_UP), "toe_angle_sum std UP: {0:.2f}".format(toe_angle_sum_std_UP), " | ",
+              "toe_angle_sum mean DOWN: {0:.2f} ".format(toe_angle_sum_mean_DOWN), "toe_angle_sum std DOWN: {0:.2f}".format(toe_angle_sum_std_DOWN)
+              )
 
         return
 
