1707b930d5e8b592077fbf40b39b56ba9e9dfac5
Jojo <jojo.schultz@outlook.de>
2021-06-30 08:19:30 +1000

    changed swing to stride in duty factor

--- lizardanalysis/calculations/duty_factor.py
+++ lizardanalysis/calculations/duty_factor.py
@@ -4,7 +4,7 @@ def duty_factor(**kwargs):
     - For walking, typical values are
     - For running, typical values are
 
-    :return: results dataframe with 6 key value pairs (list of frame-wise angles for every foot)
+    :return: results dataframe with 4, 6, or 8 key value pairs (list of frame-wise angles for every foot)
     """
     import numpy as np
     from lizardanalysis.utils import auxiliaryfunctions
@@ -22,7 +22,7 @@ def duty_factor(**kwargs):
     # results will be stored into a key value pair dictionary
     results = {}
     phase_counter = {}
-    swing_vs_stance = {"swing": 0, "stance": 0, "swing_frames": 0, "stance_frames": 0}
+    stride_vs_stance = {"stride": 0, "stance": 0, "stride_frames": 0, "stance_frames": 0}
 
     # obtain all columns from the current results dataframe relating to step_phase
     step_phase_list = [col for col in df_result_current.columns if ('step_phase_' in col)]
@@ -42,7 +42,7 @@ def duty_factor(**kwargs):
             phase_counter.pop("b\'nan\'")  # need to check if it's just the NAN that comes up when loaded as a df
 
             # deleting the first and last elements of the dictionary also, because you don't know if they were complete
-            # swing/stance phases, therefore they could skew the calculation of the duty factor
+            # stride/stance phases, therefore they could skew the calculation of the duty factor
 
             i = 0
             keys_to_delete = []
@@ -62,33 +62,28 @@ def duty_factor(**kwargs):
             # need to count how many swing phases and how many stance phases there are
             # then need to find the average number of frames for a given swing or stance phase
             # frame is a suitable covariant for time, as a frame has a fixed time interval
-            # finally calculate the stance/stance+swing
+            # finally calculate the stance/stance+stride
 
         for key, value in phase_counter.items():
 
-            if "swing" in key:
-                swing_vs_stance["swing"] += 1
-                swing_vs_stance["swing_frames"] += value
+            if "stride" in key:
+                stride_vs_stance["stride"] += 1
+                stride_vs_stance["stride_frames"] += value
 
             elif "stance" in key:
-                swing_vs_stance["stance"] += 1
-                swing_vs_stance["stance_frames"] += value
+                stride_vs_stance["stance"] += 1
+                stride_vs_stance["stance_frames"] += value
 
             else:
                 print("Unrecognised phase type remains in dictionary; will be excluded.")
 
-        average_swing = swing_vs_stance["swing_frames"] / swing_vs_stance["swing"]
-        average_stance = swing_vs_stance["stance_frames"] / swing_vs_stance["stance"]
+        average_swing = stride_vs_stance["stride_frames"] / stride_vs_stance["stride"]
+        average_stance = stride_vs_stance["stance_frames"] / stride_vs_stance["stance"]
         duty_f = average_stance / (average_swing + average_stance)
 
         results[foot] = duty_f
 
 
-    else:
-        print("foot does not match with current column name!")
-        break
+    results = {'average_duty_factor' + key: value for (key, value) in results.items()}
 
-
-results = {'average_duty_factor' + key: value for (key, value) in results.items()}
-
-return results
+    return results
