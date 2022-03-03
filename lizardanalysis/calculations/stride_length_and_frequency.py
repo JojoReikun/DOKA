important fix in stride length and stride frequency calculation

lizardanalysis/calculations/stride_length_and_frequency.py
@@ -35,7 +35,6 @@ def stride_length_and_frequency(**kwargs):
         active_columns.append("stepphase_{}".format(foot))
     # print("active_columns: ", active_columns)
 
-    step_phase_lengths = []
     stride_lengths = []
 
     results = {}
@@ -44,73 +43,77 @@ def stride_length_and_frequency(**kwargs):
         column = column.strip('')
         # print("column :", column)
         results[foot] = np.full((data_rows_count,), np.NAN)
-
+        #print(f"\n>>>> foot: {foot}, active column: {column}")
 
         for i in range(1, max_stride_phase_count):
             # this looks for all stride phases of the current foot
             cell_value = loop_encode(i)
+            #print(f"cell value: {cell_value}")
             df_stride_section = df_result_current[df_result_current[column] == cell_value]
+            #print(f"type of df_stride_section: {type(df_stride_section)}")
 
-            if len(df_stride_section) == 0:
+            if len(df_stride_section) == 0:     # TODO: probably not the correct way to check for 0!!
+                #print(df_stride_section is 0)
                 break
 
             # print(df_stride_section)
             df_stride_section_indices = list(df_stride_section.index.values)
 
-            # exclude all step phases which are 0 or longer than 1/10 of framerate
-            if len(df_stride_section_indices) <= float(framerate) / 100 or len(df_stride_section_indices) > float(framerate) / 10:
+            # exclude all step phases which are less than 1/100 of the framerate (500fps = 5 frames) or longer than 1/10 of framerate (500fps = 50 frames)
+            if len(df_stride_section_indices) < float(framerate) / 100 or len(df_stride_section_indices) > float(framerate) / 10:
+                #print(f"df stride section outside of margin with: {len(df_stride_section_indices)}")
                 break
 
-            step_phase_lengths.append(len(df_stride_section_indices))
+            beg_end_tuple = (df_stride_section_indices[0], df_stride_section_indices[-1])
+            #print(f"beg_end_tuple: {beg_end_tuple}")
 
-            if len(df_stride_section_indices) > 0:
-                beg_end_tuple = (df_stride_section_indices[0], df_stride_section_indices[-1])
-
+            if i > 1:  # only start at step2 because you need the previous beg_end_tuple for full stride
                 # filter for likelihood of labels:
-                likelihood_shoulder_begin = data.loc[beg_end_tuple[0]][scorer, "Shoulder", 'likelihood']
-                likelihood_shoulder_end = data.loc[beg_end_tuple[1]][scorer, "Shoulder", 'likelihood']
-                likelihood_hip_begin = data.loc[beg_end_tuple[0]][scorer, "Hip", 'likelihood']
-                likelihood_hip_end = data.loc[beg_end_tuple[1]][scorer, "Hip", 'likelihood']
+                stride_start = prev_beg_end_tuple[0]
+                stride_end = beg_end_tuple[0] + 1
+                likelihood_shoulder_begin = data.loc[stride_start][scorer, "Shoulder", 'likelihood']
+                likelihood_shoulder_end = data.loc[stride_end][scorer, "Shoulder", 'likelihood']
+                likelihood_hip_begin = data.loc[stride_start][scorer, "Hip", 'likelihood']
+                likelihood_hip_end = data.loc[stride_end][scorer, "Hip", 'likelihood']
 
                 # only includes strides where the shoulder to hip of the lizard are fully accurately tracked
                 if likelihood_shoulder_begin >= likelihood and likelihood_shoulder_end >= likelihood \
                         and likelihood_hip_begin >= likelihood and likelihood_hip_end >= likelihood:
+                    #print("likelihood checks passed! Calculating distance...")
                     # calculate the euclidean distance between last coord and current coord of shoulder and hip -> mean
-                    xdiff_shoulder = data.loc[beg_end_tuple[1]][scorer, "Shoulder", 'x'] - data.loc[beg_end_tuple[0]][
+                    xdiff_shoulder = data.loc[stride_end][scorer, "Shoulder", 'x'] - data.loc[stride_start][
                         scorer, "Shoulder", 'x']
-                    ydiff_shoulder = data.loc[beg_end_tuple[1]][scorer, "Shoulder", 'y'] - data.loc[beg_end_tuple[0]][
+                    ydiff_shoulder = data.loc[stride_end][scorer, "Shoulder", 'y'] - data.loc[stride_start][
                         scorer, "Shoulder", 'y']
                     distance_shoulder = np.sqrt(xdiff_shoulder ** 2 + ydiff_shoulder ** 2)
 
-                    xdiff_hip = data.loc[beg_end_tuple[1]][scorer, "Hip", 'x'] - data.loc[beg_end_tuple[0]][
+                    xdiff_hip = data.loc[stride_end][scorer, "Hip", 'x'] - data.loc[stride_start][
                         scorer, "Hip", 'x']
-                    ydiff_hip = data.loc[beg_end_tuple[1]][scorer, "Hip", 'y'] - data.loc[beg_end_tuple[0]][
+                    ydiff_hip = data.loc[stride_end][scorer, "Hip", 'y'] - data.loc[stride_start][
                         scorer, "Hip", 'y']
                     distance_hip = np.sqrt(xdiff_hip ** 2 + ydiff_hip ** 2)
+                    distance = get_distance_average(distance_shoulder,
+                                                    distance_hip)  # get stride length in distance (px)
+                    #print(f"distance in px: {distance}")
 
                 else:
                     distance_shoulder = np.NAN
                     distance_hip = np.NAN
-            else:
-                distance_shoulder = np.NAN
-                distance_hip = np.NAN
+                    distance = get_distance_average(distance_shoulder,
+                                                    distance_hip)  # get stride length in distance (px)
 
-            distance = get_distance_average(distance_shoulder, distance_hip)
-
-            if i > 1:
                 # saves the distance to results for the current foot, for stride1 beg until stride 2 begin
-                for row in range(prev_beg_end_tuple[0], beg_end_tuple[0] + 1):
+                for row in range(stride_start, stride_end):
                     results[foot][row] = distance
-                stride_lengths.append(distance)
 
-                #stride_lengths.append(abs(prev_beg_end_tuple[0] - (beg_end_tuple[0] + 1)))
-
+                # get length of stride in frames (time)
+                stride_lengths.append(abs(stride_start - stride_end))
 
             prev_beg_end_tuple = beg_end_tuple
 
-    # calculate the stride frequency for the entire run - could include it in loop above to get frequ. stepphase wise
-    stride_frequency = np.round(framerate/np.mean(stride_lengths), 2)
-    #print("stride_frequency: ", stride_frequency)
+    # calculate the stride frequency for the entire run:
+    stride_frequency = np.round(framerate / np.mean(stride_lengths), 2)
+    # print("stride_frequency: ", stride_frequency)
     #print("stride length: ", stride_lengths)
     #print("step phase lengths: ", step_phase_lengths)
 